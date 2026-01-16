b4631f87db4be784815f58e8c91cb5ee799581af
shree <shreeharsha437@gmail.com>
2026-01-17 01:38:47 +0530

    Remove base path from vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
 
     return {
-        base: '/',
+
         plugins: [react()],
         server: {
             port: 5173,
